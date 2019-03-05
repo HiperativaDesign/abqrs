42d6a68ca11e389006f99ea6299b2a5d12d857c8
Rodrigo rodrigues <rodrigo.scotti1@gmail.com>
2019-03-05 19:25:43 -0300

    [WIP] Estilização da página de posts

--- pages/single-page.php
+++ pages/single-page.php
@@ -1,8 +1,8 @@
 <div class="row padding-y">
 	<div class="padding-y grid-9 row-container">
 		<?php
-		$query = new WP_Query();
-		if ( $query->have_posts() ): $query->the_post(); ?>
+		the_post();
+		?>
 		<div class="row-item">
 			<div class="img-post">
 				<?php the_post_thumbnail(); ?>
@@ -20,10 +20,5 @@
 		<div class="card-footer-item">
 			<?php the_time( 'j F' ); ?>
 		</div>
-		<?php
-		wp_reset_postdata();
-		else : ?>
-		<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-		<?php endif; ?>
 	</div>
 </div>
