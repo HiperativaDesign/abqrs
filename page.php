bb018d1821b2e36195a17465b8765eefab21eed9
Rodrigo rodrigues <rodrigo.scotti1@gmail.com>
2019-03-10 19:35:05 -0300

    [WIP] Alteração do template do woocommerce

--- page.php
+++ page.php
@@ -1,6 +1,37 @@
 <?php
-get_header();
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package storefront
+ */
 
+get_header(); ?>
 
-// Footer
+	<div class="row padding-y">
+		<div class="row-container padding-y grid-9">
+			<?php
+
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+
+			endwhile; // End of the loop.
+			?>
+
+		</div>
+	</div>
+
+<?php
 get_footer();
